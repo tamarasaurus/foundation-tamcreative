f5b76804b386efac368756ebd5e318924b56a087
Tamara Robichet <tamarasaurus@users.noreply.github.com>
2019-02-12 15:41:18 +0100

    Create mod.lua

---
+++ mod.lua
@@ -0,0 +1,9 @@
+local myMod = foundation.createMod({
+	Name = "Creative Mode",
+	Author = "Tam",
+	Description = "Creative/easy mode. ",
+	Version = "0.0.1"
+});
+
+myMod:log("Tam Creative Mode loading");
+
